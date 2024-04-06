4ff335edacc9e2bf14dc36a2884133386e710ac9
Ramsheed K K <ramsheedkk06@gmail.com>
2024-04-06 21:22:56 +0530

    first commit

---
+++ config/Db.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import colors from "colors";
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URL);
+    console.log(
+      `Conneted To Mongodb Databse ${conn.connection.host}`.bgMagenta.white
+    );
+  } catch (error) {
+    console.log(`Errro in Mongodb ${error}`.bgRed.white);
+    console.log('Retrying MongoDB connection...');
+    setTimeout(() => connectDB(), 5000); // Retry after 5 seconds
+  }
+};
+
+export default connectDB;
+
+
+
