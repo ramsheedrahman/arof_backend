239278f519a12912986ddebd7646d79a1c5b14c3
Ramsheed KK <117103380+ramsheedrahman@users.noreply.github.com>
2024-04-08 20:27:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ dotenv.config();
 connectDB()
 //middelwares
 app.use(cors({
-  origin: 'https://your-react-app.vercel.app',
+  origin: 'https://ecommerce-arof.vercel.app/',
   credentials: true // if you're using cookies
 }));
 app.use(express.json({limit:'10mb'}));
